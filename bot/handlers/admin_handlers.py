ed814d088997f8905f14501df522e8a57f612d5a
wizand0 <xabor@yandex.ru>
2025-04-17 15:23:49 +0300

    v. 1.3.10 "some admin fixes"

--- bot/handlers/admin_handlers.py
+++ bot/handlers/admin_handlers.py
@@ -272,7 +272,7 @@ async def server_statistics(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         f"• Загрузка CPU: {cpu_percent}%\n"
         # f"• Uptime: {uptime_val:.0f} секунд\n" if uptime_val is not None else ""
         f"• Uptime: {uptime_str}\n"
-        f"• Температура:\n{temp_message}"
+        f"• Температура:\n{temp_message}\n"
         f"• Lan IP:\n{local_ip}"
     )
 
