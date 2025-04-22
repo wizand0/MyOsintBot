0ad4ccd1e013558644e70f05f0fdd74754a8c222
wizand0 <xabor@yandex.ru>
2025-04-22 15:21:07 +0300

    v. 1.3.17 "sphinx"

--- docker/sphinx/conf/sphinx.conf.tpl
+++ docker/sphinx/conf/sphinx.conf.tpl
@@ -1,11 +1,30 @@
-# .\docker\sphinx\sphinx.conf.tpl
-source src_osint
+source src1
 {
- type          = mysql
- sql_host      = ${DB_HOST}
- sql_user      = ${DB_USER}
- sql_pass      = ${DB_PASSWORD}
- sql_db        = ${DB_NAME}
- sql_port      = ${DB_PORT}
- …
+    type            = mysql
+    sql_host        = {{ DB_HOST }}
+    sql_user        = {{ DB_USER }}
+    sql_pass        = {{ DB_PASSWORD }}
+    sql_db          = {{ DB_NAME }}
+    sql_port        = {{ DB_PORT }}
+    sql_query       = SELECT id, title, content FROM documents
+}
+
+index idx_main
+{
+    source          = src1
+    path            = /var/lib/sphinx/data/idx_main
+}
+
+searchd
+{
+    listen          = 9306:mysql41
+    log             = /var/log/sphinx/searchd.log
+    query_log       = /var/log/sphinx/query.log
+    pid_file        = /var/run/sphinx/searchd.pid
+    max_children    = 30
+    seamless_rotate = 1
+    preopen_indexes = 1
+    unlink_old      = 1
+    workers         = threads
+    binlog_path     = /var/lib/sphinx/data
 }
--- docker/sphinx/scripts/start-sphinx.sh
+++ docker/sphinx/scripts/start-sphinx.sh
@@ -1,23 +1,18 @@
-#!/usr/bin/env sh
+#!/bin/sh
 set -e
 
-# 1) Ждём поднятия MariaDB
-until mysql -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" \
-    -e "SHOW DATABASES;" &>/dev/null
-do
-  echo "Waiting for MariaDB…"
+# --- опционально: ждём базу данных
+echo "Waiting for DB at $DB_HOST..."
+until mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
+  echo -n "."
   sleep 2
 done
+echo "DB is up!"
 
-# 2) Подставляем переменные в шаблон
-envsubst '${DB_HOST} ${DB_USER} ${DB_PASSWORD} ${DB_NAME} ${DB_PORT}'  \
-  < /etc/sphinx/sphinx.conf.tpl \
-  > /etc/sphinx/sphinx.conf
+# --- генерируем реальный конфиг
+envsubst < /etc/sphinx/sphinx.conf.tpl > /etc/sphinx/sphinx.conf
+echo "Using sphinx config:"
+cat /etc/sphinx/sphinx.conf
 
-# 3) Индексируем
-indexer --all --rotate
-
-# 4) Запускаем демона
+# --- запускаем демона
 exec searchd --nodetach --config /etc/sphinx/sphinx.conf
-# docker exec -it sphinx_search sh mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASSWORD" -e "SHOW DATABASES;"
-
