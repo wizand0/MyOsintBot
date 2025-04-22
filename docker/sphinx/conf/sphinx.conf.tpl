13492555d2abd9f3e77b1fe57d707de4af452e02
wizand0 <xabor@yandex.ru>
2025-04-22 16:22:32 +0300

    v. 1.3.17 "sphinx"

--- docker/sphinx/conf/sphinx.conf.tpl
+++ docker/sphinx/conf/sphinx.conf.tpl
@@ -1,20 +1,20 @@
 source src1
 {
-    type            = mysql
-    sql_host        = $DB_HOST
-    sql_user        = $DB_USER
-    sql_pass        = $DB_PASSWORD
-    sql_db          = $DB_NAME
-    sql_port        = $DB_PORT
-    sql_query       = SELECT id, title, content FROM documents
+    type     = mysql
+    sqlhost = mariadbserver
+    sql_user = wizand0
+    sql_pass = Pol2E16Li3O4
+    sqldb   = osintbd
+    sql_port = 3306
+    sql_query= SELECT id, title, content FROM documents
 }
 
 index idx_main
-   {
-     source    = src1
-     datadir   = /var/lib/sphinx/data
-     indexname = idx_main
-   }
+{
+    source = src1
+    # единый путь + базовое имя; .sph, .spi, .spd и т.п. сформируются автоматически
+    path   = /var/lib/sphinx/data/idx_main
+}
 
 searchd
 {
@@ -22,7 +22,6 @@ searchd
     pid_file        = /tmp/searchd.pid
     log             = /tmp/searchd.log
     query_log       = /tmp/query.log
-    max_children    = 30
     seamless_rotate = 1
     preopen_indexes = 1
     unlink_old      = 1
--- docker/sphinx/scripts/start-sphinx.sh
+++ docker/sphinx/scripts/start-sphinx.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
-# .\docker\sphinx\scripts\start-sphinx.sh
 set -e
 
-# --- опционально: ждём базу данных
-echo "Waiting for DB at $DB_HOST..."
-until mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
-  echo -n "."
+# 1) Ждём, пока БД будет доступна
+echo "Waiting for DB at ${DB_HOST}:${DB_PORT:-3306}..."
+until mysql -h"$DB_HOST" -P"${DB_PORT:-3306}" -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1" > /dev/null 2>&1; do
+  printf '.'
   sleep 2
 done
-echo "DB is up!"
+echo "\nDB is up!"
 
-# --- генерируем реальный конфиг
+# 2) Рендерим шаблон в реальный конфиг
 envsubst < /etc/sphinx/sphinx.conf.tpl > /etc/sphinx/sphinx.conf
-echo "Using sphinx config:"
+echo "Generated /etc/sphinx/sphinx.conf:"
 cat /etc/sphinx/sphinx.conf
 
-# --- запускаем демона
+# 3) Прогоняем initial-indexer (можно без --rotate,
+#    если индексы ещё не заведены)
+echo "Running indexer..."
+indexer --config /etc/sphinx/sphinx.conf --all --rotate
+
+# 4) Запускаем демона внутри контейнера (foreground)
+echo "Starting searchd..."
 exec searchd --nodetach --config /etc/sphinx/sphinx.conf
